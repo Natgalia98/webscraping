8fcac705ced994849715819404ab81ed16c43870
Nathalia Garces <nathalia.garces98@gmail.com>
2025-10-13 09:15:46 -0300

    Add files via upload

---
+++ ifmg.py
@@ -0,0 +1,28 @@
+import requests
+
+#responsavel por fazer a requisição ao endereço da internet e obter o conteudo da pagina
+
+from bs4 import BeautifulSoup
+
+#'-> resposável por pegar o codigo HTML
+# que o requests obteve da pagina e transformar em um objeto python
+
+import re
+
+#request e BeautifulSoup
+# variaveis que requisitam o que procuram no site
+
+tecnologia_ifmg=requests.get('https://mais.ifmg.edu.br/maisifmg/course/index.php?categoryid=10')
+
+url_curso_ifmg='/course/view.php?id='
+
+curso_tecnologia_ifmg=BeautifulSoup(tecnologia_ifmg.text,'html.parser')
+
+print("\nCurso de Informática - IFMG\n")
+
+for link in curso_tecnologia_ifmg.find_all('a'):
+    url=link.get('href')
+    nome_curso=link.get_text()
+    if  url and url_curso_ifmg in url and nome_curso:
+        print(nome_curso)
+        print(url)
